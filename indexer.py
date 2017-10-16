5647636385b456f9171cc01ec8ea455ac79862fc
sasha-pivovarov <cantwork.pivovarov@gmail.com>
2017-10-16 20:48:17 +0300

    Add files via upload

---
+++ indexer.py
@@ -0,0 +1,46 @@
+import pymorphy2
+import re, string, pickle, os
+class ReverseIndex:
+    pregex = re.compile('[%s]' % re.escape(string.punctuation))
+    stoppos = ["PREP", "CONJ"]
+    def __init__(self):
+        self.words = {}
+        self.length = 0
+        self.analyzer = pymorphy2.MorphAnalyzer()
+
+    def add(self, text:str):
+        textpart = text.split("\n\n")[-1]
+        tokens = [self.analyzer.normal_forms(self.pregex.sub('', x)) for x in textpart.split() if not any([tag._POS in self.stoppos for tag in self.analyzer.tag(x)])]
+        tokens = [token for toklist in tokens for token in toklist]
+        for token in tokens:
+            self.words.setdefault(token, set()).add(self.length)
+        self.length += 1
+        print("Added a text")
+
+    def search(self, query:str):
+        pass
+
+    def load(self, name="index.pkl"):
+        with open(name, 'rb') as loadfile:
+            tmp_dict = pickle.load(loadfile)
+
+        self.__dict__.update(tmp_dict)
+
+    def save(self, name="index.pkl"):
+        with open(name, 'wb') as savefile:
+            pickle.dump(self.__dict__, savefile, 2)
+
+if __name__ == "__main__":
+    paths = [x for x in os.listdir('.') if x.endswith(".txt")]
+    index = ReverseIndex()
+    for path in paths:
+        with open(path) as text:
+            content = text.read()
+            index.add(content)
+
+    index.save()
+
+    test_in = ReverseIndex()
+    test_in.load()
+
+    print("done")
